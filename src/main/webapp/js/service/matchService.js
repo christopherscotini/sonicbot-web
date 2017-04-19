ba8fb0ede2835ff07e4a8749d7e2e6b2608fdfc6
Christopher Rozario <christopher.rosari.old@buscapecompany.com>
2017-04-19 17:51:11 -0300

    nova versao

--- src/main/webapp/js/service/matchService.js
+++ src/main/webapp/js/service/matchService.js
@@ -1,20 +1,18 @@
 (function() {
 	'use strict';
 
-	app.service('MatchService', MatchService); // Define o nome a função do seu
-											// .service
-
+	app.service('MatchService', MatchService);
+	
 	MatchService.$inject = [ '$http' ]; // Lista de dependências
-//	var REST_SERVICE_URI = 'https://sbcollector.herokuapp.com/sbcollector/matches/list?date=2017-04-18';
-	var REST_SERVICE_URI = 'http://localhost:8082/sbcollector/matches/list?date=2017-04-18';
+	
+	var REST_SERVICE_URI = 'https://sbcollector.herokuapp.com/sbcollector/matches/list?date=2017-04-18';
+//	var REST_SERVICE_URI = 'http://localhost:8082/sbcollector/matches/list?date=2017-04-18';
 
 	function MatchService($http) {
 
 		var vm = this;
 		vm.listarJogos = listarJogos;
 
-		// Implementação das funções
-		
 		function listarJogos() {
 			$http({
 				method : 'GET',
@@ -24,4 +22,5 @@
 			});
 		}
 	}
+	
 })();
