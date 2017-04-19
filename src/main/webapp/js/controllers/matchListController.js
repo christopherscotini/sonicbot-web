f5f887d8ddf6ae0166f06e2c435aae7e2f4bebce
Christopher Rozario <christopher.rosari.old@buscapecompany.com>
2017-04-19 18:24:21 -0300

    Concluindo primeiro exemplo de serviço

--- src/main/webapp/js/controllers/matchListController.js
+++ src/main/webapp/js/controllers/matchListController.js
@@ -3,14 +3,22 @@
  
     app.controller('MatchListController', MatchListController);
  
-    matchListController.$inject = ['MatchService']; // Injetamos o service
+    matchListController.$inject = [ 'MatchService' ]; // Injetamos o service
  
     /* @ngInject */
     //Injetamos o service
     function MatchListController(MatchService) {
+    
+    	
     	var vm = this;
     	vm.title = 'Jogos do Dia';
-
-    	vm.matches = MatchService.listarJogos(); 
+    	
+    	MatchService.listarJogos().then(function(responseData) {
+    		vm.matches = responseData;
+		}, function(errResponse) {
+			console.error('Error while fetching Matches');
+		});
+    	
+    	
     }
 })();
--- src/main/webapp/js/service/matchService.js
+++ src/main/webapp/js/service/matchService.js
@@ -3,24 +3,27 @@
 
 	app.service('MatchService', MatchService);
 	
-	MatchService.$inject = [ '$http' ]; // Lista de dependências
+	MatchService.$inject = [ '$http', '$q' ]; // Lista de dependências
 	
 	var REST_SERVICE_URI = 'https://sbcollector.herokuapp.com/sbcollector/matches/list?date=2017-04-18';
 //	var REST_SERVICE_URI = 'http://localhost:8082/sbcollector/matches/list?date=2017-04-18';
 
-	function MatchService($http) {
+	function MatchService($http, $q) {
 
 		var vm = this;
 		vm.listarJogos = listarJogos;
 
 		function listarJogos() {
-			$http({
-				method : 'GET',
-				url : REST_SERVICE_URI
-			}).then(function(response) {
-				return response.data;
+			var deferred = $q.defer();
+				 $http.get(REST_SERVICE_URI).then(function(response) {
+				deferred.resolve(response.data);
+			}, function(errResponse) {
+				console.error('Error while list games');
+				deferred.reject(errResponse);
 			});
+			return deferred.promise;
 		}
+		
 	}
 	
 })();
